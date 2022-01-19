1e3d1f5fee818d2797b00b44d6f4be4be6f70670
Hazard <hazard9909@gmail.com>
2022-01-19 21:32:33 +0100

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    import "../app.css"
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	let display = false;
+	onMount(() => {
+		display = true;
+	});
+</script>
+
+{#if display}
+	<!-- <Header /> -->
+	<!-- {#key $page} -->
+	<main in:fade>
+		<slot />
+	</main>
+	<!-- {/key} -->
+	<!-- <Footer /> -->
+{/if}
+
+<style>
+    
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import Leo from "./leo.svelte";
+
+
+</script>
+
+<Leo />
+
+<style>
+
+</style>
