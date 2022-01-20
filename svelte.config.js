25617b9efa7cff29d069947850d95575c044ae65
Hazard <hazard9909@gmail.com>
2022-01-21 00:12:05 +0100

    change adapter to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
+export default {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: cloudflare({
+			// any esbuild options
+		})
 	}
 };
-
-export default config;
