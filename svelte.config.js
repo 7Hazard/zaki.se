16fa880230dcedc43c672010505d7c596ac56a16
Hazard <hazard9909@gmail.com>
2022-01-21 00:52:12 +0100

    Added svelte preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import cloudflare from '@sveltejs/adapter-cloudflare';
+import preprocess from 'svelte-preprocess';
 
 export default {
+	preprocess: preprocess(),
 	kit: {
 		target: '#svelte',
 		adapter: cloudflare({
